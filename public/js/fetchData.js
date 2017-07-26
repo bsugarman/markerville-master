b220db1ec10afe53678ee4c59a612893b2aa5938
bsugarman <brandms@stanford.edu>
2017-07-26 16:54:45 -0700

    Update fetchData.js

--- public/js/fetchData.js
+++ public/js/fetchData.js
@@ -8,10 +8,11 @@ async function onSearchData(event) {
   const json = await result.json();
   const resultWord = json.word;
   const associated = json.associated;
-
+  var count = 0;
   if(associated.length != 0 && word.length!=0){
 
   for (let obj in associated){
+    count++;
     let row = document.createElement('tr');
 
     let name = document.createElement('td');
@@ -37,6 +38,7 @@ async function onSearchData(event) {
 
     table.appendChild(row);
     }
+    console.log(count);
 
 }
 }
